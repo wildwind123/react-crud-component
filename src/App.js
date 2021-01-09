fdeaf2f83f764c173e9d91e9c889497b520857bf
wildwind <z-petr-i@mail.ru>
2021-01-09 12:40:20 +0900

    default page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Switch from "react-bootstrap/esm/Switch";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import User from "./Page/Users/User";
 
 class App extends Component {
@@ -11,6 +11,9 @@ class App extends Component {
                     <Route path="/user" component={User} exact/>
                     <Route path="/user/:id" component={User} exact/>
                     <Route path="/user/:id/:action" component={User} exact/>
+                    <Route >
+                        <Redirect to="/user"/>
+                    </Route>
                 </Switch>
             </BrowserRouter>
         );
